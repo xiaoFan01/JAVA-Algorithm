aff8665480c8acbd30e0f6052eb426986825c452
xiaoFan01 <1300456010@qq.com>
2019-01-20 13:35:25 +0800

    栈内元素反转（借助队实现）

--- src/JavaInterview/Test7.java
+++ src/JavaInterview/Test7.java
@@ -1,4 +1,25 @@
 package JavaInterview;
 
 public class Test7 {
+    public static void main(String[] args) {
+        MyStack stack = new MyStack();
+        for (int i = 1; i < 6; i++){
+            stack.push(i);
+        }
+        MyQueue queue = new MyQueue();
+        System.out.println("初始栈内元素：");
+        while (stack.size()>0){
+            System.out.print(stack.top() + " ");
+            queue.enQueue(stack.pop());
+        }
+        while (queue.size()>0){
+            stack.push(queue.getFront());
+            queue.deQueue();
+        }
+        System.out.println();
+        System.out.println("反转后栈内元素：");
+        while (stack.size()>0){
+            System.out.print(stack.pop() + " ");
+        }
+    }
 }
